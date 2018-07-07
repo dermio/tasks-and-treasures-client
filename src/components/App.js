6761302ecf09769fbf94e55c7a684e0250b55897
dermio <thisdjay@gmail.com>
2018-07-07 11:48:58 -0700

    Added LoginForm component to App render

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import  LoginForm  from "./login-form";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         Howdy
+        <LoginForm />
       </div>
     );
   }
