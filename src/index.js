811b6561823796c6ce4d5f5e04ff2cc0a7a47b20
dermio <thisdjay@gmail.com>
2018-07-06 18:01:15 -0700

    Imported & used store Provider and react-router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./components/App";
+import store from "./store";
 import "./index.css";
 
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 registerServiceWorker();
