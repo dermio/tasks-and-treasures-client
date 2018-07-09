e6add750136c128f1f0714be92c77e913e5d5bd9
dermio <thisdjay@gmail.com>
2018-07-08 21:10:55 -0700

    Created LandingPage component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import { connect } from "react-redux";
+import { Route, /* withRouter */ Redirect } from "react-router-dom";
+
 import './App.css';
 
-import  LoginForm  from "./login-form";
+import LogoutButton from "./logout-button";
+import LoginForm  from "./login-form";
+
+
+// landingPage component will eventually be moved to its own file.
+function landingPage(props) {
+  let { isLoggedIn } = props;
+  // If we are logged in redirect straight to the user's dashboard
+  if (isLoggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div className="home">
+      <h2>Welcome to Foo App</h2>
+      <LoginForm />
+      {/* <Link to="/register">Register</Link> */}
+    </div>
+  );
+}
+
+const LandingPage = connect(mapStateToProps)(landingPage);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         Howdy
-        <LoginForm />
+        {this.props.isLoggedIn && <LogoutButton />}
+        <Route exact path="/" component={LandingPage} />
       </div>
     );
   }
 }
 
-export default App;
+
+// const mapStateToProps = (state) => ({
+//   isLoggedIn: !!state.auth.currentUser
+// });
+
+function mapStateToProps(state) { // function declaration is hoisted
+  return {
+    isLoggedIn: !!state.auth.currentUser
+  };
+}
+
+export default connect(mapStateToProps)(App);
