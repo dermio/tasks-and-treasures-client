21ab62869e2027cb123538e82152856ef29aba84
dermio <thisdjay@gmail.com>
2018-07-11 16:14:29 -0700

    Created LandingPage component in landing-page.js

---
+++ src/components/landingPage.js
@@ -0,0 +1,29 @@
+import react from "react";
+import { connect } from "react-redux";
+import { Link, Redirect } from "react-router-dom";
+
+import LoginForm from "./login-form";
+
+export function LandingPage(props) {
+  let { isLoggedIn } = props;
+  // If we are logged in redirect straight to the user's dashboard
+  if (isLoggedIn) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <div className="home">
+      <h2>Welcome to Foo App</h2>
+      <LoginForm />
+      {/* <Link to="/register">Register</Link> */}
+    </div>
+  );
+}
+
+/* Line 18, Register? */
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: !!state.auth.currentUser
+});
+
+export default connect(mapStateToProps)(landingPage);
