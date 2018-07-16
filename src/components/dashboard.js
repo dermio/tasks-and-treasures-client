665fac1557572901103180253fdb3dfaf6f393ba
dermio <thisdjay@gmail.com>
2018-07-16 12:55:21 -0700

    Created Dashboard component

---
+++ src/components/dashboard.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { connect } from "redux";
+
+// import dashboard.css
+import LogoutButton from "./logout-button";
+
+export class Dashboard extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <p>USER'S DASHBOARD</p>
+        <LogoutButton />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoggedIn: !!state.auth.currentUser
+  // isLoggedIn: state.auth.currentUser !== null
+});
+
+export default connect(mapStateToProps)(Dashboard);
