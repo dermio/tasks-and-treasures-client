78c019a9d063e1fcc8b7d897d3743d38aa6cc8ec
dermio <thisdjay@gmail.com>
2018-07-23 21:08:33 -0700

    Added Redirect to LandingPage if NOT logged in

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Redirect } from "react-router-dom";
 
 // import dashboard.css
 import LogoutButton from "./logout-button";
@@ -7,6 +8,10 @@ import LogoutButton from "./logout-button";
 export class Dashboard extends React.Component {
 
   render() {
+    if (!this.props.isLoggedIn) {
+      return <Redirect to="/" />
+    }
+
     return (
       <div>
         <p>USER'S DASHBOARD</p>
