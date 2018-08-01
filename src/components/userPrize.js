3c522745bddf48af7655facf1a7afa7194970b4e
dermio <thisdjay@gmail.com>
2018-08-01 13:36:50 -0700

    Added componentDidMount method to dispatch fetchPrize

--- src/components/userPrize.js
+++ src/components/userPrize.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
 
-//import { fetchPrize } from "../actions/prizes";
+import { fetchPrize } from "../actions/prizes";
 
 export class UserPrize extends React.Component {
+  componentDidMount() {
+    // console.log("[[[ UserPrize mounted ]]]");
+    this.props.dispatch(fetchPrize("mariobros"));
+  }
+
   render() {
     return <div>Box of Oreos</div>
   }
