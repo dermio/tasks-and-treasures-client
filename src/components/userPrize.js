86bf50a307cf4c800b67ea517b301abc2144ab88
dermio <thisdjay@gmail.com>
2018-08-01 00:18:34 -0700

    Imported connect and fetchPrize component

--- src/components/userPrize.js
+++ src/components/userPrize.js
@@ -1,5 +1,7 @@
 import React from "react";
-//import { connect } from "react-redux";
+import { connect } from "react-redux";
+
+import { fetchPrize } from "../actions/prizes";
 
 export class UserPrize extends React.Component {
   render() {
