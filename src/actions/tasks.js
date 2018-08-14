0ebc37b564b94338e6390fe1949b1dc6f9492ed3
dermio <thisdjay@gmail.com>
2018-08-13 21:58:44 -0700

    Created getTasksRequest action creator

--- src/actions/tasks.js
+++ src/actions/tasks.js
@@ -1,6 +1,11 @@
 import { API_BASE_URL } from "../config";
 import { normalizeResponseErrors } from "./utils";
 
+export const GET_TASKS_REQUEST = "GET_TASKS_REQUEST";
+export const getTasksRequest = ({
+  type: GET_TASKS_REQUEST
+});
+
 export const GET_TASKS_SUCCESS = "GET_TASKS_SUCCESS";
 export const getTasksSuccess = data => ({
   type: GET_TASKS_SUCCESS,
