ffb16e07b7361c08b56da242845fe5e4f4744631
dermio <thisdjay@gmail.com>
2018-08-13 22:54:49 -0700

    Initial state & reducer handle action type GET tasks request

--- src/reducers/tasks.js
+++ src/reducers/tasks.js
@@ -1,21 +1,30 @@
 import {
-  GET_TASKS_ERROR,
-  GET_TASKS_SUCCESS
+  GET_TASKS_REQUEST,
+  GET_TASKS_SUCCESS,
+  GET_TASKS_ERROR
 } from "../actions/tasks";
 
 const initialState = {
-  allUserTasks: [],
+  loading: false,
+  allUserTasks: [], // the data, success
   error: null
 };
 
 export default function reducer(state = initialState, action) {
+  if (action.type === GET_TASKS_REQUEST) {
+    return Object.assign({}, state, {
+      loading: true
+    });
+  }
   if (action.type === GET_TASKS_SUCCESS) {
     return Object.assign({}, state, {
+      loading: false,
       allUserTasks: action.data,
       error: null
     });
   } else if (action.type === GET_TASKS_ERROR) {
     return Object.assign({}, state, {
+      loading: false,
       error: action.error
     });
   }
