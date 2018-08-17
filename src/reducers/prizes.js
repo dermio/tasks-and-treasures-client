ab3d7f837d2b971a9d86a2aff98846345295ed50
dermio <thisdjay@gmail.com>
2018-08-17 12:24:58 -0700

    Edited prizes reducer to handle GET prize request
    - Imported GET_PRIZE_REQUEST string.
    - Added a pending GET prize property to the initial state.
    - Added a condition to the reducer to handle if the action type
    is a GET prize request.
    - Edited the block statements for the GET prize success and error
    conditions to set pending GET prize property to false.

--- src/reducers/prizes.js
+++ src/reducers/prizes.js
@@ -1,21 +1,29 @@
 import {
+  GET_PRIZE_REQUEST,
   GET_PRIZE_SUCCESS,
   GET_PRIZE_ERROR
 } from "../actions/prizes";
 
 const initialState = {
+  pending_GET_prize: false,
   userPrize: null,
   error: null
 };
 
 export default function reducer(state = initialState, action) {
-  if (action.type === GET_PRIZE_SUCCESS) {
+  if (action.type === GET_PRIZE_REQUEST) {
     return Object.assign({}, state, {
+      pending_GET_prize: true
+    });
+  } else if (action.type === GET_PRIZE_SUCCESS) {
+    return Object.assign({}, state, {
+      pending_GET_prize: false,
       userPrize: action.data,
       error: null
     });
   } else if (action.type === GET_PRIZE_ERROR) {
     return Object.assign({}, state, {
+      pending_GET_prize: false,
       error: action.error
     });
   }
