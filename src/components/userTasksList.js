1029dd5553c2f10e40fececdb3ddced0765d0b5a
dermio <thisdjay@gmail.com>
2018-08-28 16:41:30 -0700

    Imported updateTask thunk and Task component

--- src/components/userTasksList.js
+++ src/components/userTasksList.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 
+import Task from "./task";
 import "./userTasksList.css";
 
-import { getTasks, deleteTask } from "../actions/tasks";
+import { getTasks, deleteTask, updateTask } from "../actions/tasks";
 
 export class UserTasksList extends React.Component {
   componentDidMount() {
@@ -15,14 +16,29 @@ export class UserTasksList extends React.Component {
     this.props.dispatch(deleteTask(task.id));
   }
 
+  onUpdate = (event, task) => {
+    event.preventDefault();
+    console.log("click UPDATE task button");
+    this.props.dispatch(updateTask(task.id));
+  }
+
   render() {
     let tasks = this.props.userTasks.map((task, index) =>
-      <li key={index}>
-        {task.taskName}
-        <button onClick={(e) =>{this.onDelete(e, task)}}>
-          Delete Task
-        </button>
-      </li>
+      // <li key={index}>
+      //   {task.taskName}
+      //   <button onClick={(e) =>{this.onDelete(e, task)}}>
+      //     Delete Task
+      //   </button>
+      //   <button onClick={(e) =>{this.onUpdate(e, task)}}>
+      //     Update Task
+      //   </button>
+      // </li>
+      <Task
+        key={index}
+        task={task}
+        onDelete={(e) =>{this.onDelete(e, task)}}
+        onUpdate={(e) =>{this.onUpdate(e, task)}}
+      />
     );
 
     return (
