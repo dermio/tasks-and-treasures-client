2e2a0bee8fa48bd4fc65929188346ea538afbe13
dermio <thisdjay@gmail.com>
2018-09-08 19:00:14 -0700

    Completed UpdateTaskForm component
    - Imported compose.
    - Wrote `onSubmit` method to handle dispatch/updating tasks,
    and created onSubmit event handler for form element.
    - Created cancel button to cancel updating tasks.
    - Used compose to pass connect(mapStateToProps) and
    reduxForm to enhance the store?

--- src/components/update-task-form.js
+++ src/components/update-task-form.js
@@ -1,12 +1,29 @@
 import React from "react";
+import { compose } from 'redux';
+import { connect } from "react-redux";
 import { Field, reduxForm /*, focus */} from "redux-form";
 
+import { updateTask } from "../actions/tasks";
+
+
 export class UpdateTaskForm extends React.Component {
+  onSubmit(values) {
+    console.log(values);
+    this.props.dispatch(updateTask({
+      taskName: values.taskname,
+      id: this.props.task.id,
+      onTaskUpdated: this.props.onTaskUpdated
+    }));
+  }
 
   render() {
     return (
-      <form>
-        <label htmlFor="taskname">Task Name</label>
+      <form
+        onSubmit={
+          this.props.handleSubmit(values => this.onSubmit(values))
+        }
+      >
+        {/* <label htmlFor="taskname">Task Name</label> */}
         <Field
           component="input"
           type="text"
@@ -14,11 +31,37 @@ export class UpdateTaskForm extends React.Component {
           id="taskname"
         />
         <button type="Submit Task">Submit Task</button>
+        <button type="button" onClick={this.props.onTaskUpdated} >
+          Cancel Update
+        </button>
       </form>
     );
   }
 }
 
+/* // old
+const mapStateToProps = (state, ownProps) => ({
+  initialValues: {
+    taskname: ownProps.task.taskName
+  }
+});
+
 export default reduxForm({
   form: "updateTask"
-})(UpdateTaskForm);
+}, mapStateToProps)(UpdateTaskForm); */
+
+
+/* // jshin
+export default compose( connect((state, props) => ({ initialValues: {
+  taskname: props.task.taskName
+} })), reduxForm({ form: "updateTask" }) )(UpdateTaskForm); */
+
+
+
+export default compose(
+  // mapStateToProps is argument for connect
+  connect((state, props) => ({
+    initialValues: { taskname: props.task.taskName }
+  })),
+  reduxForm({ form: "updateTask" })
+)(UpdateTaskForm);
