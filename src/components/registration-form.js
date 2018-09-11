e29bd20f34a41d4b5041a528b2389c1e69092369
dermio <thisdjay@gmail.com>
2018-09-11 12:03:58 -0700

    Imported validators, set password length

--- src/components/registration-form.js
+++ src/components/registration-form.js
@@ -3,6 +3,10 @@ import { Field, reduxForm, /* focus */ } from "redux-form";
 
 // import Input from "./input";
 
+import { required, nonEmpty, isTrimmed, length, matches } from "../validators";
+
+const passwordLength = length({ min: 10, max: 72 });
+
 export class RegistrationForm extends React.Component {
 
   render() {
