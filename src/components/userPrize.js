5858d963f6341a231ecfcaa9297a1ee99a02d26d
dermio <thisdjay@gmail.com>
2018-10-16 11:44:21 -0700

    Deleted code to delete, update, & create prize
    - Deleted commented code to dispatch createPrize, createOrUpdatePrize,
    and deletePrize from the console through the window object.
    
    /* Temporarily added dispatch and createPrize as properties to
    the window object. Will use the methods to test creating a prize
    from the browser console. */
    window.dispatch = this.props.dispatch;
    window.createOrUpdatePrize = createOrUpdatePrize;
    // window.createPrize = createPrize; OLD code if createPrize imported
    
    window.deletePrize = deletePrize;
    
    /* Run the following code, after the two previous lines of code,
    to dispatch creating a prize in the browser console.
    dispatch(createPrize({familyCode: "mariobros", prizeName: "Disneyland"}))
    <- OR ->
    dispatch(createPrize({
      familyCode: "mariobros", prizeName: "Disneyland"
    }))
    */

--- src/components/userPrize.js
+++ src/components/userPrize.js
@@ -1,30 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import { getPrize, createOrUpdatePrize, deletePrize } from "../actions/prizes";
+import { getPrize, deletePrize } from "../actions/prizes";
 import ConnectedShowIfRoleIs from "./ShowIfRoleIs";
 
 export class UserPrize extends React.Component {
   componentDidMount() {
     this.props.dispatch(getPrize());
-
-    /* Temporarily added dispatch and createPrize as properties to
-    the window object. Will use the methods to test creating a prize
-    from the browser console. */
-    window.dispatch = this.props.dispatch;
-    window.createOrUpdatePrize = createOrUpdatePrize;
-    // window.createPrize = createPrize; OLD code if createPrize imported
-
-    window.deletePrize = deletePrize;
-
-    /* Run the following code, after the two previous lines of code,
-    to dispatch creating a prize in the browser console.
-    dispatch(createPrize({familyCode: "mariobros", prizeName: "Disneyland"}))
-    <- OR ->
-    dispatch(createPrize({
-      familyCode: "mariobros", prizeName: "Disneyland"
-    }))
-    */
   }
 
   onDelete(event, prize) {
