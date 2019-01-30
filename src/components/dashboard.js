350d7f27ffd94789b341a098a39874d1279e6d9d
dermio <thisdjay@gmail.com>
2019-01-29 19:56:23 -0800

    Removed tasks and prize methods from Dashboard

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -20,30 +20,6 @@ export class Dashboard extends React.Component {
     };
   }
 
-  // Task button shown is local to Dashboard component. Dashboard
-  // is responsible for this state. No need to use Redux.
-  onAddTaskButtonClick() { // Click create Task, show Create Task form
-    this.setState({
-      isAddTaskFormVisible: !this.state.isAddTaskFormVisible
-    });
-  }
-
-  onAddPrizeButtonClick() { // Click create Prize, show Create Prize Form
-    this.setState({
-      isAddPrizeFormVisible: !this.state.isAddPrizeFormVisible
-    });
-  }
-
-  onFinalizeTasksList() {
-    console.log("[[[ CLICK FINALIZE TASKS LIST ]]]");
-    this.props.dispatch(finalizeTasksList());
-  }
-
-  onResetTasksList() {
-    console.log("[[[ CLICK RESET TASKS LIST ]]]");
-    this.props.dispatch(resetTasksList());
-  }
-
   render() {
     if (!this.props.isLoggedIn) {
       return <Redirect to="/" />
