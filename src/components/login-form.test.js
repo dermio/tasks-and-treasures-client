9dfdae10a81bb8728f8aafcc9474495b1a37a7cc
dermio <thisdjay@gmail.com>
2019-04-04 14:01:29 -0700

    Added shallow render test for LoginForm

---
+++ src/components/login-form.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import LoginForm from "./login-form";
+
+
+describe("<LoginForm />", () => {
+  it("Renders without crashing", () => {
+    shallow(<LoginForm />);
+  });
+
+  // it("Renders the `Login` button initially", () => {
+  //   let wrapper = shallow(<LoginForm />);
+  //   expect(wrapper.hasClass("login-form")).toEqual(true);
+  //   expect(wrapper.hasClass('add-button')).toEqual(true);
+
+  // });
+});
+
