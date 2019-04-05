d62e47086982c724c2db32389a857dbf8809fba6
dermio <thisdjay@gmail.com>
2019-04-05 12:27:59 -0700

    Added shallow test for CreateTaskForm

---
+++ src/components/create-task-form.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import CreateTaskForm from "./create-task-form";
+
+
+describe("<CreateTaskForm />", () => {
+  it("Renders without crashing", () => {
+    shallow(<CreateTaskForm />);
+  });
+
+});
+
