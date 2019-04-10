335b6302148f3fb6cb5c8d64c230e0f8b03f7cef
dermio <thisdjay@gmail.com>
2019-04-10 11:58:36 -0700

    Added shallow test for LogoutButton

---
+++ src/components/logout-button.test.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { shallow, mount } from "enzyme";
+
+import { LogoutButton } from "./logout-button";
+
+
+describe("<LogoutButton />", () => {
+  it("Renders without crasing", () => {
+    shallow(<LogoutButton />);
+  });
+});
+
+
+/*
+Import no bracket
+Invariant Violation: Could not find "store" in either the context or props
+of "Connect(LogoutButton)". Either wrap the root component in a <Provider>,
+or explicitly pass "store" as a prop to "Connect(LogoutButton)".
+
+
+Import with bracket
+It Works!
+*/
