8eb83fefb18d72af199752cc085e6099208d0405
dermio <thisdjay@gmail.com>
2019-04-19 21:09:59 -0700

    Imported named CreateTaskForm component

--- src/components/create-task-form.test.js
+++ src/components/create-task-form.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 
-import CreateTaskForm from "./create-task-form";
+import { CreateTaskForm } from "./create-task-form";
 
 
 describe("<CreateTaskForm />", () => {
