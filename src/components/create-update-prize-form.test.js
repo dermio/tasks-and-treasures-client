c1f139a139c5c321964d16c6b91df7462424f052
dermio <thisdjay@gmail.com>
2019-04-19 21:12:53 -0700

    Imported named CreateOrUpdatePrizeForm component, corrected test

--- src/components/create-update-prize-form.test.js
+++ src/components/create-update-prize-form.test.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { shallow, mount } from "enzyme";
 
-import CreateOrUpdatePrizeForm from "./create-update-prize-form";
+import { CreateOrUpdatePrizeForm } from "./create-update-prize-form";
 import { isTrimmed } from "../validators";
 
 
 describe("<CreateOrUpdatePrizeForm />", () => {
   it("Renders without crashing", () => {
-    shallow(<CreateOrUpdatePrizeForm />);
+    shallow(
+      <CreateOrUpdatePrizeForm
+        handleSubmit={() => {}}
+      />
+    );
   });
 });
 
