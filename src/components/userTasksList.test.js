05bb5718587b29e7eba8e59a67fa481de22587aa
dermio <thisdjay@gmail.com>
2019-04-25 10:49:37 -0700

    Added commented code for mount & ShowIfRoleIs

--- src/components/userTasksList.test.js
+++ src/components/userTasksList.test.js
@@ -3,6 +3,8 @@ import { shallow, mount } from "enzyme";
 
 import { UserTasksList } from "./userTasksList";
 
+import ShowIfRoleIs from "./ShowIfRoleIs";
+
 
 describe("<UserTasksList />", () => {
   it("Renders without crashing", () => {
@@ -13,6 +15,29 @@ describe("<UserTasksList />", () => {
       />
     );
   });
+
+
+  // it("ShowIfRoleIs wrapper", () => {
+
+  //   const WrapperShowIfRoleIs = mount(
+  //     <ShowIfRoleIs
+  //       userRole="parent"
+  //     />
+  //   );
+
+  //   // wrapper.debug()
+
+  //   const wrapper = shallow(
+  //     <UserTasksList
+  //       userTasks={[]}
+  //       dispatch={() => {}}
+  //     />
+  //   );
+
+  //   console.log("Wrapper.Debug", wrapper.debug())
+  // });
 });
 
+
+/* Ray says, Wrap the userTasks List, in the Higher order componenet. */
 
