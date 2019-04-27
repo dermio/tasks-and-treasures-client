3de9bac3b394682a67fb4b1d116e5d1440cc8403
dermio <thisdjay@gmail.com>
2019-04-27 12:23:48 -0700

    Added commented code heroku

--- src/config.js
+++ src/config.js
@@ -1,2 +1,9 @@
 export const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
+
+/*
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  "https://tasks-and-treasures-api.herokuapp.com/api" ||
+  "http://localhost:8080/api";
+*/
