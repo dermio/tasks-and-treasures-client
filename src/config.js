a264f16b3080c6bd4888b4fdb5ab3a77e83d9579
dermio <thisdjay@gmail.com>
2019-04-27 17:02:08 -0700

    Changed order of client API_BASE_URL
    Changed order of client React API_BASE_URL. The localhost:8080
    serves as the client API_BASE_URL when developing locally.
    When the app is deployed, the client uses an API_BASE_URL
    that points to the API server running on Heroku.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,10 @@
-export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
-
 /*
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "https://tasks-and-treasures-api.herokuapp.com/api" ||
-  "http://localhost:8080/api";
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080/api";
 */
+
+export const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL ||
+  "http://localhost:8080/api" ||
+  "https://tasks-and-treasures-api.herokuapp.com/api";
+
