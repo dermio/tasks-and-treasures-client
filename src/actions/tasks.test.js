48428f5589181496bbdf9ce0cf7679478f9763bf
dermio <thisdjay@gmail.com>
2019-05-04 16:36:19 -0700

    Tested action creators for DELETE & UPDATE tasks success

--- src/actions/tasks.test.js
+++ src/actions/tasks.test.js
@@ -1,6 +1,8 @@
 import {
   GET_TASKS_SUCCESS, getTasksSuccess,
-  CREATE_TASK_SUCCESS, createTaskSuccess
+  CREATE_TASK_SUCCESS, createTaskSuccess,
+  DELETE_TASK_SUCCESS, deleteTaskSuccess,
+  UPDATE_TASK_SUCCESS, updateTaskSuccess
 } from "./tasks";
 
 describe("createTaskSuccess", () => {
@@ -35,6 +37,31 @@ describe("getTasksSuccess", () => {
 
     expect(action.type).toEqual(GET_TASKS_SUCCESS);
     expect(action.data).toEqual(data);
+  });
+});
+
+describe("deleteTaskSuccess", () => {
+  it("Should return the action for Delete Task success", () => {
+    const data = {
+      status: 204
+    };
+    const action = deleteTaskSuccess(data);
+
+    expect(action.type).toEqual(DELETE_TASK_SUCCESS);
+    expect(action.data.status).toEqual(204);
+  });
+});
+
+describe("updateTaskSuccess", () => {
+  it("Should return the action for Update Task success", () => {
+    const data = {
+      taskName: "homework", // Updated taskName
+      familyCode: "smith"
+    };
+    const action = updateTaskSuccess(data);
+
+    expect(action.type).toEqual(UPDATE_TASK_SUCCESS);
+    expect(action.data.taskName).toEqual(data.taskName);
   });
 });
 
